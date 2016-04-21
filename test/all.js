ae37a845e72e0a59244c6553bb901d50bdc9cdfc
Waylon Flinn <waylonflinn@gmail.com>
2016-04-21 12:40:31 -0500

    integrate markdown-it-testgen

--- test/all.js
+++ test/all.js
@@ -1,9 +1,26 @@
-var tape = require('tape'),
+var path = require('path'),
+	tape = require('tape'),
+	testLoad = require('markdown-it-testgen').load,
 	mdk = require('../index');
 
+var md = require('markdown-it')()
+	.use(mdk);
 
-tape("hello tape", function(t){
-	t.plan(1);
+/* this uses the markdown-it-testgen module to automatically generate tests
+   based on an easy to read text file
+ */
+testLoad(path.join(__dirname, 'fixtures/default.txt'), function(data){
+	data.fixtures.forEach(function (fixture){
 
-	t.assert(true, "it works!");
+		tape(fixture.header, function(t){
+			t.plan(1);
+
+			var expected = fixture.second.text,
+				actual = md.render(fixture.first.text);
+
+			t.equals(actual, expected);
+
+			//generate(, { "assert" : t.assert }, md);
+		});
+	});
 });
